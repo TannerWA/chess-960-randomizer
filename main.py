993c2c5e41dc6558773dafbf812703a7f99a1748
TannerWA <Tannerpmr11@gmail.com>
2022-01-08 01:21:16 -0500

    Add license

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+"""
+    Randomizes the back rank of chess pieces and provides a graphical 
+    representation to the user.
+    
+    Copyright (C) 2022  Tanner Allen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from tkinter import *
+
 from board.board import Board
+
+
 def main() -> None:
     def fill_canvas(canvas: Canvas, board: Board) -> None:
         canvas.create_image(20, 20, anchor = NW, image = board.board_image)
